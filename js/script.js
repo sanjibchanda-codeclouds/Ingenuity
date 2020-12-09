c83aa8f5a6398369c48fd810fabdeceb29c86678
Sanjib Chanda <sanjib.chanda@codeclouds.in>
2020-12-09 20:00:14 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+/* slider navbar
+.............................*/
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+/* sticky navbar
+.............................*/
+window.onscroll = function() {myFunction()};
+  var header = document.getElementById("header");
+  var sticky = header.offsetTop;
+
+  function myFunction() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  }
+
+  
