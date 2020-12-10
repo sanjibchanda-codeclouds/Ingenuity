615654db183f19a32037da8e99541c29c697858b
Sanjib Chanda <sanjib.chanda@codeclouds.in>
2020-12-10 10:06:48 +0530

    scroll to top script added

--- js/script.js
+++ js/script.js
@@ -24,4 +24,24 @@ window.onscroll = function() {myFunction()};
     }
   }
 
-  
+
+/* scroll to top script
+.............................*/
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+});  
