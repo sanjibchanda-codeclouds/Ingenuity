280cbaf85bd3b4c55c6f5c6fa6b1886330f78fca
sanjibchanda-codeclouds <sanjib.chanda@codeclouds.in>
2020-12-13 14:42:46 +0530

    added preloader

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+//preloader
+$(window).bind("load", function () {
+  jQuery("#loader").fadeOut("slow");
+  jQuery("#preloader").delay(0).fadeOut();
+});
+
 
 /* slider navbar
 .............................*/
